ce4dd6edb121bf8dc201d1c961753bb156863f95
Maurya Suryakant <mauryasuryakant99@gmail.com>
2026-09-15 14:17:13 +0530

    new updates

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,27 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('DevSmith is active');
+	const provider = new DevSmithProvider();
+
+	context.subscriptions.push(
+		vscode.window.registerTreeDataProvider(
+			'devsmith.hello',
+			provider
+		)
+	);
+}
+
+class DevSmithProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+
+	getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(): vscode.TreeItem[] {
+		return [
+			new vscode.TreeItem('Hello from devsmith')
+		];
+	}
 }
 
 export function deactivate() {}
